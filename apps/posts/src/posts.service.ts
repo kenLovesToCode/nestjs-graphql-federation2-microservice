2f6049d54a9004e2c9d5301ecdde9ed5245e66b3
Jovanne Vicentino <jvicentino@contractors.stevensonsystems.com>
2023-04-26 13:02:42 +0800

    initial

---
+++ apps/posts/src/posts.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostInput } from './dto/create-post.input';
+import { Post } from './entities/post.entity';
+// import { UpdatePostInput } from './dto/update-post.input';
+
+@Injectable()
+export class PostsService {
+  private readonly posts: Post[] = [
+    { id: '001', authorId: '001', body: 'First Post' },
+    { id: '002', authorId: '002', body: 'Second Post' },
+    { id: '003', authorId: '001', body: 'Third Post' },
+  ];
+
+  create(createPostInput: CreatePostInput) {
+    this.posts.push(createPostInput);
+    return createPostInput;
+  }
+
+  findAll() {
+    return this.posts;
+  }
+
+  findOne(id: string) {
+    return this.posts.find((post) => post.id === id);
+  }
+
+  forAuthor(authorId: string) {
+    return this.posts.filter((post) => post.authorId === authorId);
+  }
+  // update(id: number, updatePostInput: UpdatePostInput) {
+  //   return `This action updates a #${id} post`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} post`;
+  // }
+}
---
+++ apps/users/src/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: '001',
+      email: 'kenlovestocode@gmail.com',
+      password: 'supersecurepassword',
+    },
+    {
+      id: '002',
+      email: 'kenlovestoplay@gmail.com',
+      password: 'supersecurepassword',
+    },
+  ];
+  create(createUserInput: CreateUserInput) {
+    this.users.push(createUserInput);
+    return createUserInput;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  // update(id: number, updateUserInput: UpdateUserInput) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
